1812729162e3a4c3a8a7612b3b8adfae70cb3432
Daniel Wang <danielwpz@gmail.com>
2023-06-06 14:47:02 +0800

    cloudflare

---
+++ functions/markets.ts
@@ -0,0 +1,28 @@
+// import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { fetchMarketsData } from "../utils/markets";
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
+// const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+//   const res = await fetchMarketsData();
+//   return {
+//     statusCode: 200,
+//     headers,
+//     body: JSON.stringify(res),
+//   };
+// };
+
+// export { handler };
+
+interface Env {
+	KV: KVNamespace;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const res = await fetchMarketsData();
+ 	return new Response(JSON.stringify(res));
+}
