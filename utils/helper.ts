f9b44699223503f05ab3eab686acbe0d8661b838
Daniel Wang <danielwpz@gmail.com>
2023-06-12 15:44:13 +0800

    add zkevm testnet

--- utils/helper.ts
+++ utils/helper.ts
@@ -23,6 +23,11 @@ export const chainConfig: {
     uiPoolDataProviderAddress: markets.AaveV3Polygon.UI_POOL_DATA_PROVIDER,
     lendingPoolAddressProvider: markets.AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
   },
+  [ChainId.zkevm_testnet]: {
+    providerRPC: "https://rpc.public.zkevm-test.net",
+    uiPoolDataProviderAddress: "0x6D09af48575BacfbAafF1e32d3e102929D2a8a6B",
+    lendingPoolAddressProvider: "0xa7945c8Fbde87F847DC2058074fb7b7E469Bf23a",
+  }
 }
 
 export function getTimestamp() {
