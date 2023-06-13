9a67ac1198c39d1396fcb8aaeebf3bbfef73ed39
Daniel Wang <danielwpz@gmail.com>
2023-06-13 11:26:36 +0800

    update chainid

--- utils/helper.ts
+++ utils/helper.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@aave/contract-helpers';
 import * as markets from '@bgd-labs/aave-address-book';
 
 export const chainConfig: {
-  [key in ChainId]?: {
+  [key: number]: {
     providerRPC: string,
     uiPoolDataProviderAddress: string,
     lendingPoolAddressProvider: string,
@@ -23,7 +23,7 @@ export const chainConfig: {
     uiPoolDataProviderAddress: markets.AaveV3Polygon.UI_POOL_DATA_PROVIDER,
     lendingPoolAddressProvider: markets.AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
   },
-  [ChainId.zkevm_testnet]: {
+  [1442]: {
     providerRPC: "https://rpc.public.zkevm-test.net",
     uiPoolDataProviderAddress: "0x6D09af48575BacfbAafF1e32d3e102929D2a8a6B",
     lendingPoolAddressProvider: "0xa7945c8Fbde87F847DC2058074fb7b7E469Bf23a",
