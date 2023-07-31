6e6ea1d854b4fb44750ad19aef03ef7d3c32c564
Daniel Wang <danielwpz@gmail.com>
2023-07-31 11:14:14 +0800

    feat: content type

--- functions/[chain]/health/[user].ts
+++ functions/[chain]/health/[user].ts
@@ -7,6 +7,7 @@ const headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "Content-Type",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  "Content-Type": "application/json;charset=UTF-8",
 };
 
 interface Env {
--- functions/[chain]/markets.ts
+++ functions/[chain]/markets.ts
@@ -1,10 +1,10 @@
-import { ChainId } from "@aave/contract-helpers";
 import { fetchMarketsData } from "../../utils/markets";
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "Content-Type",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  "Content-Type": "application/json;charset=UTF-8",
 };
 
 interface Env {
