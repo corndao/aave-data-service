5500dcda18bfe18b9e57942562475fdb8b938565
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2023-11-04 10:36:02 +0000

    Better logs and error handling in health_factor.ts

--- utils/health_factor.ts
+++ utils/health_factor.ts
@@ -21,16 +21,34 @@ export async function calculateHealthFactor(
   action: UserAction,
   amountUSD: number
 ): Promise<string> {
+  // Ensure asset is a non-empty string
+  if (typeof asset !== 'string' || asset.trim() === '') {
+    console.error("Invalid asset parameter:", asset); // Log the invalid asset
+    throw new Error("Asset must be a non-empty string");
+  }
+
+  console.log(`Calculating health factor for asset: ${asset}`); // Log the valid asset
+
   const userSummary = await fetchUserSummary(chainId, userAddress);
   const reserves = await fetchFormattedPoolReserves(chainId);
 
+  // Log all available asset IDs in reserves for debugging
+  console.log("All asset IDs in reserves:", reserves.map(r => r.underlyingAsset));
+
+  let lowerCaseAsset = asset.toLowerCase();
+  // Log the asset ID you're searching for
+  console.log("Searching for asset ID:", lowerCaseAsset);
+
   const targetReserve = _.find(
     reserves,
-    (r) => r["underlyingAsset"] === asset.toLowerCase()
+    (r) => r["underlyingAsset"] === lowerCaseAsset
   );
+
   if (!targetReserve) {
+    console.error(`Asset ID not found: ${lowerCaseAsset}`);
     throw new Error("bad asset id");
   }
+
   if (
     (action === UserAction.Deposit || action === UserAction.Withdraw) &&
     (!targetReserve.usageAsCollateralEnabled || targetReserve.isIsolated)
@@ -38,49 +56,35 @@ export async function calculateHealthFactor(
     return userSummary.healthFactor;
   }
 
-  const reserveLiquidationThreshold =
-    parseInt(targetReserve.reserveLiquidationThreshold) / 10000;
-
+  const reserveLiquidationThreshold = parseInt(targetReserve.reserveLiquidationThreshold) / 10000;
   const totalCollateralUSD = valueToBigNumber(userSummary.totalCollateralUSD);
-  const totalBorrowsUSD = valueToBigNumber(
-    userSummary.totalBorrowsMarketReferenceCurrency
-  );
+  const totalBorrowsUSD = valueToBigNumber(userSummary.totalBorrowsMarketReferenceCurrency);
 
   let totalCollateralUSDAfter = totalCollateralUSD;
   let totalBorrowsUSDAfter = totalBorrowsUSD;
-  let liquidationThresholdAfter = valueToBigNumber(
-    userSummary.currentLiquidationThreshold
-  );
+  let liquidationThresholdAfter = valueToBigNumber(userSummary.currentLiquidationThreshold);
 
   switch (action) {
-    case UserAction.Deposit: {
+    case UserAction.Deposit:
       totalCollateralUSDAfter = totalCollateralUSD.plus(amountUSD);
       liquidationThresholdAfter = totalCollateralUSD
         .multipliedBy(valueToBigNumber(userSummary.currentLiquidationThreshold))
-        .plus(
-          valueToBigNumber(amountUSD).multipliedBy(reserveLiquidationThreshold)
-        )
+        .plus(valueToBigNumber(amountUSD).multipliedBy(reserveLiquidationThreshold))
         .dividedBy(totalCollateralUSDAfter);
       break;
-    }
-    case UserAction.Withdraw: {
+    case UserAction.Withdraw:
       totalCollateralUSDAfter = totalCollateralUSD.minus(amountUSD);
       liquidationThresholdAfter = totalCollateralUSD
         .multipliedBy(valueToBigNumber(userSummary.currentLiquidationThreshold))
-        .minus(
-          valueToBigNumber(amountUSD).multipliedBy(reserveLiquidationThreshold)
-        )
+        .minus(valueToBigNumber(amountUSD).multipliedBy(reserveLiquidationThreshold))
         .dividedBy(totalCollateralUSDAfter);
       break;
-    }
-    case UserAction.Borrow: {
+    case UserAction.Borrow:
       totalBorrowsUSDAfter = totalBorrowsUSD.plus(amountUSD);
       break;
-    }
-    case UserAction.Repay: {
+    case UserAction.Repay:
       totalBorrowsUSDAfter = totalBorrowsUSD.minus(amountUSD);
       break;
-    }
   }
 
   const newHealthFactor = calculateHealthFactorFromBalancesBigUnits({
