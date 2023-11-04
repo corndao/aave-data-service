665f1d9940bf80b99acfd6149a90295ec11a9153
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2023-11-04 10:51:18 +0000

    Contract addresses for Spark deployments, and privacy-preserving RPCs

--- utils/helper.ts
+++ utils/helper.ts
@@ -9,29 +9,26 @@ export const chainConfig: {
     walletBalanceProvider: string;
   };
 } = {
-  [ChainId.arbitrum_one]: {
-    providerRPC: "https://arb-mainnet-public.unifra.io",
-    uiPoolDataProviderAddress: markets.AaveV3Arbitrum.UI_POOL_DATA_PROVIDER,
-    lendingPoolAddressProvider: markets.AaveV3Arbitrum.POOL_ADDRESSES_PROVIDER,
-    walletBalanceProvider: markets.AaveV3Arbitrum.WALLET_BALANCE_PROVIDER,
-  },
+  //Hardcoded the addresses of Spark protocol deployments
+  //No Github 'address book' for Spark Protocol available
   [ChainId.mainnet]: {
-    providerRPC: "https://eth-rpc.gateway.pokt.network",
-    uiPoolDataProviderAddress: markets.AaveV3Ethereum.UI_POOL_DATA_PROVIDER,
-    lendingPoolAddressProvider: markets.AaveV3Ethereum.POOL_ADDRESSES_PROVIDER,
-    walletBalanceProvider: markets.AaveV3Ethereum.WALLET_BALANCE_PROVIDER,
+    providerRPC: "https://eth-pokt.nodies.app",
+    uiPoolDataProviderAddress: "0xF028c2F4b19898718fD0F77b9b881CbfdAa5e8Bb",
+    lendingPoolAddressProvider: "0x02C3eA4e34C0cBd694D2adFa2c690EECbC1793eE",
+    walletBalanceProvider: "0xd2AeF86F51F92E8e49F42454c287AE4879D1BeDc",
   },
+  [100]: {
+    providerRPC: "https://gnosis-pokt.nodies.app",
+    uiPoolDataProviderAddress: "0xF028c2F4b19898718fD0F77b9b881CbfdAa5e8Bb",
+    lendingPoolAddressProvider: "0xA98DaCB3fC964A6A0d2ce3B77294241585EAbA6d",
+    walletBalanceProvider: "0xd2AeF86F51F92E8e49F42454c287AE4879D1BeDc",
+  },
+  //Keep for debugging purposes - This relates to Aave v3 on Polygon
   [ChainId.polygon]: {
     providerRPC: "https://rpc.ankr.com/polygon",
     uiPoolDataProviderAddress: markets.AaveV3Polygon.UI_POOL_DATA_PROVIDER,
     lendingPoolAddressProvider: markets.AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
     walletBalanceProvider: markets.AaveV3Polygon.WALLET_BALANCE_PROVIDER,
-  },
-  [1442]: {
-    providerRPC: "https://rpc.public.zkevm-test.net",
-    uiPoolDataProviderAddress: "0x6D09af48575BacfbAafF1e32d3e102929D2a8a6B",
-    lendingPoolAddressProvider: "0xa7945c8Fbde87F847DC2058074fb7b7E469Bf23a",
-    walletBalanceProvider: "0x0da6DCAd2bE4801b644AEE679e0AdE008bB4bc6b",
   },
 };
 
